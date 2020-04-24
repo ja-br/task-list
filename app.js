d5f09e736b6cea5b481405a5e4bdb6e0031c6779
ja-br <letstalk@jamesbrooks.tech>
2020-04-24 11:07:03 -0400

    Add Task appends new li with task name

--- app.js
+++ app.js
@@ -4,18 +4,31 @@ const taskInput = document.querySelector('#task')
 const taskList = document.querySelector('.collection')
 const clearBtn = document.querySelector('.clear-tasks')
 
-loadEventListeners()
 
 const loadEventListeners = () => {
   form.addEventListener('submit', addTask)
 }
 
 const addTask = (e) => {
+
   if (taskInput.value === '') {
     alert('Add a task')
   }
 
+  const li = document.createElement('li')
+  li.classList.add('collection-item')
+  li.appendChild(document.createTextNode(taskInput.value))
 
+  const link = document.createElement('a')
+  link.classList.add('delete-item', 'secondary-content')
+  link.innerHTML = `<i class='fa fa-remove'></i>`
+  li.appendChild(link)
+
+  taskList.appendChild(li)
+
+  taskInput.value = ''
 
   e.preventDefault()
 }
+
+loadEventListeners()
