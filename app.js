4986b05969550614c2906d4207ecb59e19a17593
ja-br <letstalk@jamesbrooks.tech>
2020-04-24 10:55:59 -0400

    Made event listener for submit button

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const form = document.querySelector('#task-form')
-const taskList = document.querySelector('.collection')
-const clearBtn = document.querySelector('.clear-tasks')
 const filter = document.querySelector('#filter')
 const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('.collection')
+const clearBtn = document.querySelector('.clear-tasks')
+
+loadEventListeners()
+
+const loadEventListeners = () => {
+  form.addEventListener('submit', addTask)
+}
+
+const addTask = (e) => {
+  if (taskInput.value === '') {
+    alert('Add a task')
+  }
+
+
+
+  e.preventDefault()
+}
